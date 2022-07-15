4fba172b1cf4752ed3e5a29fd6653a405c7d465d
mickrueg <mickrueg@gmail.com>
2022-07-14 18:43:43 -0600

    Setup background image for desktop and mobile

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import MealPrepMain from './Components/MealPrep/MealPrepMain';
-import mobileBackground from './assets/mobileBackground.jpg';
 import SearchMain from './Components/Search/SearchMain';
 import InfoMain from './Components/Info/InfoMain';
 
 function App() {
   return (
     <div className="App">
-      <img className='background-image' src={mobileBackground} alt='background'/>
-      <MealPrepMain />
-      <SearchMain />
-      <InfoMain />
+        <div className='background-image'></div>
+        <div className='background-overlay'></div>
+        <MealPrepMain />
+        <SearchMain />
+        <InfoMain />
     </div>
   );
 }
