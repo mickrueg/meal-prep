9aa939903d06bbe57cdce11cafccf2adb460debb
mickrueg <mickrueg@gmail.com>
2022-07-17 12:38:50 -0600

    Added fetch with API database

--- src/Components/Search/SearchResults.js
+++ src/Components/Search/SearchResults.js
@@ -6,8 +6,51 @@ import { InfoContext } from '../Info/InfoContext';
 
 
 const SearchResults = () => {
+
+    const [displayResults, setDisplayResults] = useState()
+
+    useEffect(()=>{
+        fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=pizza&app_id=${process.env.REACT_APP_ID}&app_key=${process.env.REACT_APP_KEY}`)
+        .then(res=>res.json())
+        .then(res=>{
+            console.log("Fetch")
+            const searchResultsArray = res.hits;
+            setDisplayResults(
+                searchResultsArray.map((e, index)=>{
+                    return(
+                        <div className='resultContainer' key={index}>
+                            <div className='imageContainer'>
+                            <img 
+                                src={e.recipe.image}
+                                onError={({currentTarget})=>{
+                                    currentTarget.onerror = null;
+                                    console.log("Meal image inaccessible (403 Error). Display NA placeholder.")
+                                    currentTarget.src=imageNA;
+                                }}
+                                alt='pizza' 
+                                className='resultImage'/>
+                            </div>
+                            <div className='textContainer'>
+                                <h2 className='label'>{e.recipe.label}</h2>
+                                <div className='buttonContainer'>
+                                    <span className='greenButton' onClick={()=>{openIngredientPanel(e)}}>Ingredients</span>
+                                </div>
+                                <div className='buttonContainer'>
+                                    <span className='greenButton'>Recipe</span>
+                                </div>
+                                <div></div>
+                            </div>
+                        </div>
+                    )
+                })
+            )
+        })
+        .catch(console.error)
+    },[])
+    
     
     const searchResultsArray = searchResults.hits;
+    // const searchResultsArrayTwo = searchResults;
     const { infoState, setInfoState,
         mealImage, setMealImage,
         mealLabel, setMealLabel,
@@ -28,7 +71,8 @@ const SearchResults = () => {
     // })
     return (
         <div className='SearchResults'>
-            {searchResultsArray.map((e,index)=>{
+            {displayResults}
+            {/* {searchResultsArray.map((e,index)=>{
 
                 return(
                     <div className='resultContainer' key={index}>
@@ -53,7 +97,7 @@ const SearchResults = () => {
                         </div>
                     </div>
                 )
-            })}
+            })} */}
         </div>
     );
 };
